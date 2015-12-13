6e291cb736d108e6764079f245d9b7fa1a63eb14
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-13 23:34:50 +0200

    Add empty project - Pepenele

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿/*Doi prieteni au cumpărat un pepene de X kg, și doresc să îl împartă. Singura lor dorință e ca fiecare să 
+primească un număr par de kg din pepene. E ok și dacă nu primesc aceeași cantitate.
+Scrie un program care răspunde cu DA dacă pepenele poate fi împărțit cum își doresc cei doi prieteni,
+și cu NU în caz contrar.*/
+
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
