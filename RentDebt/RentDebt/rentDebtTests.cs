1d23f41a904159a80a2a0a156daa30409f160a98
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-14 22:12:36 +0200

    Empty rent debt project

---
+++ RentDebt/RentDebt/rentDebtTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentDebt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
