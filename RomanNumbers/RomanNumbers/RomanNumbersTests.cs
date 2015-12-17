b06a8ccea43908f1f303f6f295cc3f9712e8ea2f
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-17 13:53:32 +0200

    Added new empty RomanNumbers project

---
+++ RomanNumbers/RomanNumbers/RomanNumbersTests.cs
@@ -0,0 +1,15 @@
+﻿/*Convertește un număr dat, între 1 și 100, în cifre romane.*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumbers
+{
+    [TestClass]
+    public class RomanNumbersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
