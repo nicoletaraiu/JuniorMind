bae6e069a79244bc84b4804144a9ac8bb22e9c3b
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-21 14:29:05 +0200

    Added new empty SquaresOnChessBoard project

---
+++ SquaresOnChessBoard/SquaresOnChessBoard/SquaresonChessBoardTests.cs
@@ -0,0 +1,16 @@
+﻿/*Calculează câte pătrate se pot forma pe o tablă de șah de dimensiunea de 8 x 8.*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquaresOnChessBoard
+{
+    [TestClass]
+    public class SquaresonChessBoardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
