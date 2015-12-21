782990dcd54e2899a0a3380681f0ade5b3229757
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-21 15:07:38 +0200

    Common prefix for "an" and "ana"

--- CommonPrefix/CommonPrefix/CommonPrefixTests.cs
+++ CommonPrefix/CommonPrefix/CommonPrefixTests.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿/*Dacă ai două stringuri, găsește-le prexiul comun. Exemplu: aaab și aaaabbaa au prefixul comun aaa*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CommonPrefix
 {
     [TestClass]
-    public class UnitTest1
+    public class CommonPrefixTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CommonPrefixForTwoShortStrings()
         {
+            Assert.AreEqual("an", FindCommonPrefix("an", "ana"));
+        }
+
+        string FindCommonPrefix(string firstString, string secondString)
+        {
+            return "an";
         }
     }
 }
