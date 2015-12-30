f5e35fec7530e60e16a814e57d029f4685a6ca99
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-30 11:36:18 +0200

    Checked if the first three letters are in the word

--- Panagram/Panagram/PanagramTests.cs
+++ Panagram/Panagram/PanagramTests.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿/*Panagram
+Un panagram e o frază care conține toate literele din alfabet (englezesc). Scrie un program 
+care determină dacă o frază e o panagram.
+Exemplu: The quick brown fox jumps over the lazy dog*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Panagram
 {
     [TestClass]
-    public class UnitTest1
+    public class PanagramTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfTheFirst3LettersAppear()
         {
+            Assert.AreEqual("Yes", CheckIfPangram("abc12"));      }
+
+        string CheckIfPangram(string phrase)
+        {
+            if ((phrase.IndexOf("a") != -1) && (phrase.IndexOf("b") != -1) && (phrase.IndexOf("c") != -1))
+                return "Yes";
+            else
+                return "No";
         }
     }
 }
