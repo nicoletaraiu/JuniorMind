1fb25a24cb2c74b41167beb48a7b84a727dc3774
nicoletaraiu <nicoleta.raiu@gmail.com>
2015-12-30 12:40:16 +0200

    Calculated number of anagrams for "abc"

---
+++ Anagrams/Anagrams/AnagramTests.cs
@@ -0,0 +1,24 @@
+﻿/*Anagrame
+Pentru un cuvânt dat calculează numărul de anagramări posibile. Nu trebuie ca cuvintele
+rezultate să existe în dicționar. Anagramările nu trebuie să se repete și nu trebuie 
+generate anagramările, doar să se calculeze numărul lor.*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrams
+{
+    [TestClass]
+    public class AnagramTests
+    {
+        [TestMethod]
+        public void AnagramsForAShortWord()
+        {
+            Assert.AreEqual(6, CalculateNumberOfAnagrams("abc"));
+        }
+        int CalculateNumberOfAnagrams(string word)
+        {
+            return 3*2*1; 
+        }
+    }
+}
