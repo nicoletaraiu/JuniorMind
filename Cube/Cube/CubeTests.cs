82df55b2fdb6370b10931dbc44824217965fe6ea
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-08 00:27:02 +0200

    Added empty Cube project

---
+++ Cube/Cube/CubeTests.cs
@@ -0,0 +1,20 @@
+﻿/*Cub
+Un om de știință vrea să știe toate numerele al căror cub se termină cu 888. Ajută-l prin 
+a-i face un program care pentru numărul k, întoarce al k-lea număr al cărui cub e 888.
+
+Exemplu: dacă k e 1, rezultatul e 192*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cube
+{
+    [TestClass]
+    public class CubeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
