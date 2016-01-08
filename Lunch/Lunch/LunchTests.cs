723ba8f53b13ba8d76038cbfa80329c81e8ceb76
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-08 03:56:45 +0200

    DaysUntilNextMeetingForDays2and4

--- Lunch/Lunch/LunchTests.cs
+++ Lunch/Lunch/LunchTests.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿/*Masa de prânz
+Tu și prietenul tău vă întâlniți întâmplător la restaurant. În timpul mesei de prânz din 
+discuția avută cu el, afli că el ia prânzul la acel restaurant din 4 în 4 zile. Când te 
+vei întâlni din nou cu el, dacă tu iei prânzul la restaurantul respectiv odată la 6 zile?*/
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lunch
 {
     [TestClass]
-    public class UnitTest1
+    public class LunchTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DaysUntilNextMeetingForDays2and4()
         {
+            Assert.AreEqual(4, GetNumberOfDaysUntilTheNextLunchMeeting(2, 4));
+        }
+
+       int  GetNumberOfDaysUntilTheNextLunchMeeting(int hisDay, int myDay)
+        {
+                return myDay; 
+        
         }
     }
 }
