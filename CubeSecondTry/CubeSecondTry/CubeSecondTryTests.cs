2745db5b82c1381fc9737ff245d56745978c6b1d
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-08 02:15:54 +0200

    Added empty CubeSecondTry project
    Trying a different solution for the cube problem

---
+++ CubeSecondTry/CubeSecondTry/CubeSecondTryTests.cs
@@ -0,0 +1,19 @@
+﻿/*Cub
+Un om de știință vrea să știe toate numerele al căror cub se termină cu 888. Ajută-l prin 
+a-i face un program care pentru numărul k, întoarce al k-lea număr al cărui cub e 888.
+
+Exemplu: dacă k e 1, rezultatul e 192*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeSecondTry
+{
+    [TestClass]
+    public class CubeSecondTryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
