f9f3dbdec4b8de3f33bf6c4bc7a52d1362652ab5
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-10 12:00:22 +0200

    Added empty Lottery project

---
+++ Lottery/Lottery/LotteryTests.cs
@@ -0,0 +1,25 @@
+﻿/*Loto 6 din 49
+Vrei să participi la jocul de noroc 6 din 49 cu o singură variantă (simplă).
+
+Calculează ce șanse ai să câștigi la categoria I (6 numere)?
+
+Calculează ce șanse ai să câștigi la categoria II (5 numere)?
+
+Calculează ce șanse ai să câștigi la categoria III (4 numere)?
+
+Calculează și ce șanse ai la categoria I la 5 din 40?*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lottery
+{
+    [TestClass]
+    public class LotteryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
