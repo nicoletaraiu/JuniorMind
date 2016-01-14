d6e071e6ef1b0bf5fadc15946703a1892ca26ec8
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-14 09:17:53 +0200

    GetUniques
    Implemented and tested function for getting unique characters in a
    string

--- Anagrams/Anagrams/AnagramsTests.cs
+++ Anagrams/Anagrams/AnagramsTests.cs
@@ -1,14 +1,33 @@
-﻿using System;
+﻿/*Anagrame
+Pentru un cuvânt dat calculează numărul de anagramări posibile. Nu trebuie ca
+cuvintele rezultate să existe în dicționar. Anagramările nu trebuie să se repete 
+și nu trebuie generate anagramările, doar să se calculeze numărul lor.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Anagrams
 {
     [TestClass]
-    public class UnitTest1
+    public class AnagramsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfUniques()
         {
+            Assert.AreEqual("abcd", GetUniques("aaabbccd"));
+        }
+      
+        string GetUniques(string word)
+        {
+            string uniques = string.Empty;
+            for (int i = 0; i < word.Length; i++)
+            {
+              if (!uniques.Contains(word[i])){
+                    uniques += word[i]; 
+                }
+            }
+            return uniques;
         }
     }
 }
