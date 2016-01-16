bb7a21036d4032d326f45762354f9b80c7d5b925
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-01-16 15:01:52 +0200

    Test for first column

--- ExcelColumns/ExcelColumns/ExcelColumnsTests.cs
+++ ExcelColumns/ExcelColumns/ExcelColumnsTests.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿/*Coloane excel
+Primele 26 de coloane din Excel sunt marcate cu literele alfabetului. După care continuă cu combinații
+de câte două litere, astfel coloana 27 este AA, 28 - AB, iar coloana 52 cu AZ. După ZZ, se continuă cu 
+combinații de 3 litere.
+
+Dacă se dă numărul coloanei află care e combinația de litere corespunzătoare.*/
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ExcelColumns
 {
     [TestClass]
-    public class UnitTest1
+    public class ExcelColumnsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForFirstColumn()
         {
+            Assert.AreEqual('a', GetChar(1)); 
+        }
+
+        char GetChar( int columnNumber)
+        {
+            
+           return  (char)('a' + columnNumber - 1); 
+           
+
         }
     }
 }
