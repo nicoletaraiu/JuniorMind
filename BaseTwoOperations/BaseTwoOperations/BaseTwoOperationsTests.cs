e146fd0cd98bc730df2f7c796fa96b30f0e0fdc7
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-02-27 16:03:27 +0200

    ANDlogic function
    Added ANDlogic function and CountZeros

--- BaseTwoOperations/BaseTwoOperations/BaseTwoOperationsTests.cs
+++ BaseTwoOperations/BaseTwoOperations/BaseTwoOperationsTests.cs
@@ -34,6 +34,21 @@ namespace BaseTwoOperations
             Assert.AreEqual(1, GetAt(new byte[] { 1, 0, 1, 1 }, 3));
         }
 
+        [TestMethod]
+        public void TestCountZeros()
+        {
+            Assert.AreEqual(3, CountZeros(new byte[] { 0, 1, 0, 0, 0 }));
+            Assert.AreEqual(2, CountZeros(new byte[] { 0, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void TestANDlogic()
+        {
+            CollectionAssert.AreEqual(ConvertToBinary(1 & 11), ANDlogic(ConvertToBinary(1), ConvertToBinary(11)));
+            CollectionAssert.AreEqual(ConvertToBinary(0 & 5), ANDlogic(ConvertToBinary(0), ConvertToBinary(5)));
+            CollectionAssert.AreEqual(ConvertToBinary(25 & 3), ANDlogic(ConvertToBinary(25), ConvertToBinary(3)));
+        }
+
         byte [] ConvertToBinary(int number)
         {
             byte[] converted = new byte[0];
@@ -54,5 +69,33 @@ namespace BaseTwoOperations
         {
             return (byte)((position < binaryNo.Length) ? binaryNo[binaryNo.Length - position - 1] : 0); 
         }
+       
+        int CountZeros(byte [] binaryNo)
+        {
+            int count = 0;
+            int i = binaryNo.Length-1;
+            while (i >= 1)
+            {
+                if (binaryNo[i] == 0)
+                    count++;
+                else
+                    break;
+                i--;
+            }
+            return count;
+        }
+
+        byte[] ANDlogic(byte [] first, byte [] second)
+        {
+            byte[] result = new byte[Math.Max(first.Length, second.Length)];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte)(GetAt(first, i) * GetAt(second, i)); 
+            }
+            Array.Resize(ref result, result.Length - CountZeros(result));
+            Array.Reverse(result); 
+            return result;
+        }
+        
     }
 }
