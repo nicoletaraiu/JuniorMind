6aa0c87e09d73e527d4d64c543dad4255dfd4d05
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-02-27 12:56:07 +0200

    New BaseTwoOperations project

---
+++ BaseTwoOperations/BaseTwoOperations/BaseTwoOperationsTests.cs
@@ -0,0 +1,28 @@
+﻿/*Ai un număr zecimal transformă-l în baza doi, reprezentat ca și un array de byte (byte[]).
+
+Pentru un număr transformat implementează operațiile:
+
+NOT, AND, NOT, OR, XOR
+RightHandShift și LeftHandShift (shiftare de biți la dreapta și la stânga)
+LessThan
+Adunare și scădere
+Înmulțire și împărțire
+Folosește-te de LessThan pentru a implementa și alți operatori de comparare (GraterThan, Equal, NotEqual)
+Doar pentru numere pozitive.
+
+Poți generaliza transformarea și operațiile de la 3-6 pentru o bază aleatoare (baza fiind între 2 și 255)?*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseTwoOperations
+{
+    [TestClass]
+    public class BaseTwoOperationsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
