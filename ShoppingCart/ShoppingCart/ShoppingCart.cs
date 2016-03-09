48460ac9209395b4a66a65783cc7d73037a97d5e
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-03-09 23:13:41 +0200

    Added TotalPrice function

--- ShoppingCart/ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -1,14 +1,49 @@
-﻿using System;
+﻿/*Cumpărături
+Ai un coș plin de produse. Dacă cunoști prețul fiecărui produs în parte:
+Calculează care e totalul de plată pentru coșul cu cumpărături
+Găsește și care e cel mai ieftin produs din coș
+Elimină cel mai scump produs din coș
+Adaugă un nou produs în coș
+Calculează prețul mediu*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShoppingCart
 {
     [TestClass]
-    public class UnitTest1
+    public class ShoppingCart
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalPriceTest()
         {
+            var products = new Product[] { new Product("Soda", (decimal)6.5), new Product("Salt", (decimal)2.25), new Product ("Chips", (decimal)3.45), new Product("Apples", (decimal)4.99)};
+            Assert.AreEqual((decimal)17.19, CalculateTotalPrice(products));
         }
+
+        public struct Product
+           {
+            public string product;
+            public decimal price;
+
+            public Product(string product, decimal price)
+                {
+                   this.product = product;
+                   this.price = price;
+                }
+            }
+
+        static decimal CalculateTotalPrice(Product[] products)
+        {
+            decimal totalPrice = 0;
+            for(int i = 0; i < products.Length; i++)
+            {
+                totalPrice += products[i].price;
+            }
+            return totalPrice;
+        }
+            
+           }
+
     }
-}
+
