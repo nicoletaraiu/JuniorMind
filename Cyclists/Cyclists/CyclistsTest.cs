a556a0b5839c4d39cac4461a1a9282ec105d1e6a
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-03-23 18:13:44 +0200

    Added CalculateDistancePerSecond function

--- Cyclists/Cyclists/CyclistsTest.cs
+++ Cyclists/Cyclists/CyclistsTest.cs
@@ -1,14 +1,44 @@
-﻿using System;
+﻿/*Ciclometrul
+Un ciclometru e un mecanism simplu care numără rotațiile unei roți de bicicletă.
+În urma unei curse ai datele înregistrate de ciclometrele bicicliștilor participanți.
+Ciclometrul fiecărui participant a înregistrat numărul de rotații făcute în fiecare secundă.
+Dacă cunoști pentru fiecare bicicletă diametrul roților:
+Calculează distanța totală parcursă de toți bicicliștii
+Găsește secunda și numele biciclistului care a atins viteza maximă
+Găsește biciclistul care a avut cea mai bună viteză medie*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Cyclists
 {
     [TestClass]
-    public class UnitTest1
+    public class CyclistsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DistancePerSecondTest()
         {
+            Assert.AreEqual((decimal)376.8, CalculateDistancePerSecond(60, 2));
+        }
+
+        struct Cyclist
+        {
+            public string name;
+            public int rotationsPerSecond;
+            public int wheelDiameter; 
+
+            public Cyclist(string name, int rotationsPerSecond, int wheelDiameter)
+            {
+                this.name = name;
+                this.rotationsPerSecond = rotationsPerSecond;
+                this.wheelDiameter = wheelDiameter;
+            }
+        }
+
+        static decimal CalculateDistancePerSecond(int wheelDiameter, int rotationsPerSecond)
+        {
+            return (decimal)3.14 * wheelDiameter * rotationsPerSecond;
+
         }
     }
 }
