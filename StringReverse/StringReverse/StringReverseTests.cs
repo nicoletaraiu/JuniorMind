c57b08e0781eed8da0aff5176a0f0b075447599b
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-04-06 16:47:56 +0300

    Added ReverseString function and test

--- StringReverse/StringReverse/StringReverseTests.cs
+++ StringReverse/StringReverse/StringReverseTests.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿/*Inversare
+Inversează un string folosind recursivitatea.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace StringReverse
 {
     [TestClass]
-    public class UnitTest1
+    public class StringReverseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReverseStringTest()
         {
+            Assert.AreEqual("fedcba", ReverseString("abcdef"));
+        }
+
+        string ReverseString(string toReverse)
+        {
+            if (toReverse.Length == 1)
+                return toReverse;
+            else return toReverse[toReverse.Length-1] + ReverseString(toReverse.Substring(0, toReverse.Length -1));
         }
     }
 }
