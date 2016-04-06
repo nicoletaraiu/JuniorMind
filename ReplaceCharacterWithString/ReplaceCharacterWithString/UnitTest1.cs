4342674d302f90d403cf5924fc847d70db20f4a4
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-04-06 17:24:59 +0300

    Added ReplaceCharacter function and tests

--- ReplaceCharacterWithString/ReplaceCharacterWithString/UnitTest1.cs
+++ ReplaceCharacterWithString/ReplaceCharacterWithString/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/*Înlocuire
+Înlocuiește un caracter dintr-un string cu un alt string.*/
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ReplaceCharacterWithString
@@ -7,8 +9,18 @@ namespace ReplaceCharacterWithString
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReplaceCharacterTest()
         {
+            Assert.AreEqual("coffeecoffeecoffeecoffee", ReplaceCharacter("exam", "coffee"));
+            Assert.AreEqual("aaa", ReplaceCharacter("bbb", "a"));
+        }
+
+        string ReplaceCharacter(string initial, string replace)
+        {
+            if (initial.Length == 1)
+                return replace;
+            else
+                return replace + ReplaceCharacter(initial.Substring(0, initial.Length - 1), replace);
         }
     }
 }
