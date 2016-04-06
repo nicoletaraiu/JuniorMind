dca8e129d1c2e7022aae553df4eb17db214c5953
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-04-06 21:47:49 +0300

    Modified ReplaceCharacter function and test

---
+++ ReplaceCharacterWithString/ReplaceCharacterWithString/ReplaceCharacterTests.cs
@@ -0,0 +1,36 @@
+﻿/*Înlocuire
+Înlocuiește un caracter dintr-un string cu un alt string.*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReplaceCharacterWithString
+{
+    [TestClass]
+    public class ReplaceCharacterTests
+    {
+        [TestMethod]
+        public void ReplaceCharacterTest()
+        {
+            Assert.AreEqual("teaaat", ReplaceCharacter("test", "aaa", 's'));
+            Assert.AreEqual("ctxstx", ReplaceCharacter("casa", "tx", 'a'));
+        }
+
+        string ReplaceCharacter(string initial, string replace, char toBeReplaced)
+        {
+
+            if (initial.Length == 1)
+            {
+                if (initial[0] == toBeReplaced)
+                    return replace;
+                else
+                    return initial[0].ToString();
+            }
+
+            else
+                if (initial[initial.Length - 1] == toBeReplaced)
+                return ReplaceCharacter(initial.Substring(0, initial.Length - 1), replace, toBeReplaced) + replace;
+            else
+                return ReplaceCharacter(initial.Substring(0, initial.Length - 1), replace, toBeReplaced) + initial[initial.Length - 1];
+        }
+    }
+}
