a33428284caa1e4c076cc74b741e9790ac6eb15f
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-04-12 12:21:40 +0300

    GeneratePascalsTriangle function and tests

--- PascalsTriangle/PascalsTriangle/Pascal'sTriangleTests.cs
+++ PascalsTriangle/PascalsTriangle/Pascal'sTriangleTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*Triunghiul lui Pascal
+Generează triunghiul lui Pascal pentru un nivel dat folosindu-te de recursivitate.*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PascalsTriangle
@@ -7,8 +10,42 @@ namespace PascalsTriangle
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForTheFirstLevel()
         {
+            CollectionAssert.AreEqual(new int[] { 1 }, GeneratePascalsTriangle(1));
+            
+        }
+
+        [TestMethod]
+        public void TestForTheSecondLevel()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 1 }, GeneratePascalsTriangle(2));
+
+        }
+
+        [TestMethod]
+        public void TestForTheSixthLevel()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 5, 10, 10, 5, 1 }, GeneratePascalsTriangle(6));
+
+        }
+
+        int[] GeneratePascalsTriangle(int level)
+        {
+            int[] row = new int[level];
+            if (level == 1)
+                return new int[] { 1 };
+            else {
+                row[0] = 1;
+                row[level - 1] = 1;
+                int[] previousRow = GeneratePascalsTriangle(level - 1);
+
+                for (int i = 1; i < level - 1; i++)
+                    row[i] = previousRow[i - 1] + previousRow[i];
+
+                return row;
+            }     
+
         }
     }
 }
