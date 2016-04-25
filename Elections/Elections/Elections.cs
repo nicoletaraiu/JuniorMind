263029ccee7e0e17b067de651c064dbc403e2cb1
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-04-25 15:29:23 +0300

    Class, struct, getter, setter

---
+++ Elections/Elections/Elections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elections
+{
+    class Elections 
+    {
+        private Candidate[] ListsOfCandidates;
+
+        public Candidate [] getLists()
+        {
+            return ListsOfCandidates;
+        }
+
+        public void setLists(Candidate [] Lists)
+        {
+            ListsOfCandidates = Lists; 
+        }
+        
+       
+    }
+
+    public struct Candidate
+    {
+        string [] names ;
+        int [] noOfVotes;
+
+        public Candidate(string [] names, int [] noOfVotes)
+        {
+            this.names = names;
+            this.noOfVotes = noOfVotes;
+        }
+    }
+}
