df4e49de77c16b8de64d0854674799deedddfca6
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-05-04 14:27:29 +0300

    Sort Class and Sort test

--- Elections/Elections/ListOfCandidates.cs
+++ Elections/Elections/ListOfCandidates.cs
@@ -9,31 +9,13 @@ namespace Elections
     class ListOfCandidates
     {
         int noOfLists;
-        Candidate[] candidates; 
-        ListOfCandidates(int noOfLists, Candidate[] candidates)
+        Candidate[] candidates;
+        public ListOfCandidates(int noOfLists, Candidate[] candidates)
         {
             this.noOfLists = noOfLists;
             this.candidates = candidates;
         }
 
-     /*  private Candidate[] OrderAlphabelically(Candidate[] candidates)
-        {
-            for (int i = 1; i < candidates.Length; i++)
-            {
-                int j = i;
-                while (j > 0)
-                {
-                    if (candidates[j - 1].name >candidates[j].name)
-                    {
-                        int temp = candidates[j - 1].name;
-                        candidates[j - 1].name = candidates[j].name;
-                        candidates[j].name = temp;
-                        j--;
-                    }
-                    else
-                        break;
-                }
-                return candidates; 
-            }*/
     }
-}
+   }
+
---
+++ Elections/Elections/SortTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elections
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void SortAlphabeticallyTest()
+        {
+            var candidates = new Candidate[] {
+                new Candidate ("Ion", 314),
+                new Candidate("Adam", 207),
+                new Candidate("Dan", 500),
+                new Candidate("Vasile", 135) };
+
+            var sorted = new Candidate[] {
+                new Candidate("Adam", 207),
+                new Candidate("Dan", 500),
+                new Candidate("Ion", 314),
+                new Candidate("Vasile", 135) };
+
+            Sort S1 = new Sort(candidates);
+            CollectionAssert.AreEqual(sorted, S1.SortAlphabetically());
+        }
+
+    }
+    
+}
