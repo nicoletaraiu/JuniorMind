25456838a8475f2aa2bb6fa2d26a0ef13d975c9b
nicoletaraiu <nicoleta.raiu@gmail.com>
2016-07-07 01:47:04 +0300

    Added LinkedList class

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList<T> : IEnumerable<T>, ICollection<T>
+    {
+        private Node<T> head;
+        private int count;
+
+        public LinkedList()
+        {
+            head = new Node<T>(default(T));
+            head.Next = head;
+            head.Previous = head;
+            this.count = 0;
+
+        }
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LinkedList/LinkedList/Node.cs
+++ LinkedList/LinkedList/Node.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    class Class1
+    class Node<T>
     {
+        private T data;
+        private Node<T> next;
+        private Node<T> previous;
+
+        public Node(T data)
+        {
+            this.data = data;
+        } 
+
+        public T Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node<T> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+        public Node<T> Previous
+        {
+            get { return this.previous; }
+            set { this.previous = value; }
+        }
+
+
     }
 }
